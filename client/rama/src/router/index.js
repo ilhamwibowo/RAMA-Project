0c259c1d30f5ac72ca757bdc925080d462bf8860
AkmalArifin <makmalarifin25@gmail.com>
2023-02-16 17:58:39 +0700

    Add route

--- client/rama/src/router/index.js
+++ client/rama/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ProfileView from "../views/ProfileView.vue";
+import EditProfileView from "../views/EditProfileView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +18,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: ProfileView
+    },
+    {
+      path: "/profile/edit",
+      name: "edit-profile",
+      component: EditProfileView
     },
   ],
 });
