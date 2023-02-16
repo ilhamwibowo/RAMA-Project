9f68265864778233d1ce49fb51430716afbe4b88
tastytypist <tastytypist@users.noreply.github.com>
2023-02-16 20:46:29 +0700

    Implement SuccessfulRecovery component

---
+++ client/rama/src/components/SuccessfulRecovery.vue
@@ -0,0 +1,38 @@
+﻿<template>
+  <div class="successful-recovery">
+    <div class="title">
+      <h1>
+        Your password has been reset!
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SuccessfulRecovery",
+  
+  mounted() {
+    setTimeout(() => {
+      this.$router.push("/login");
+    }, 3000);
+  }
+}
+</script>
+
+<style scoped>
+.successful-recovery {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 0.5rem;
+}
+
+.title {
+  font-weight: 500;
+  margin-bottom: 1rem;
+  color: var(--color-heading);
+}
+</style>
