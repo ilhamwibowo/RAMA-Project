65f646c26743f7a0e6a7ce5ca78076c422161200
AkmalArifin <makmalarifin25@gmail.com>
2023-02-17 18:34:00 +0700

    feat: profile page

--- client/rama/src/components/Profile.vue
+++ client/rama/src/components/Profile.vue
@@ -2,13 +2,13 @@
     <div class="profile">
         <h1>Profile</h1>
         <div class="container-image">
-            <img :src="user.image" />
+            <img :src="previewImageUrl" />
         </div>
         <DetailProfile title="Name" :text="user.Name"/>
-        <DetailProfile title="Email" :text="user.Email"/>
-        <DetailProfile title="KTP" :text="user.KTP"/>
-        <DetailProfile title="Phone Number" :text="user.No_HP"/>
-        <DetailProfile title="Birthday" :text="user.Birthday"/>
+        <DetailProfile title="Email" :text="user.email"/>
+        <DetailProfile title="KTP" :text="user.ktp"/>
+        <DetailProfile title="Phone Number" :text="user.no_HP"/>
+        <DetailProfile title="Birthday" :text="user.birthday"/>
         <router-link :to="{ path: '/profile/edit/'}">
             <button class="btn edit">Edit</button>
         </router-link>
@@ -17,16 +17,10 @@
 
 <script>
 import DetailProfile from './DetailProfile.vue';
-// let response = await fetch('http://localhost:5226/api/User/1',
-//     {
-//         method: 'GET',
-//         mode: 'cors',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//     }).then(response => response.json());
-// const user = await fetch('http://localhost:5226/api/User/1')
-// console.log(user)
+import axios from 'axios';
+
+const env = import.meta.env
+
 export default {
     name: 'Profile',
     components: {
@@ -34,18 +28,31 @@ export default {
     },
     data() {
         return {
-            user: {
-                AccId: 1,
-                Name: 'Muhammad Akmal Arifin',
-                Email: 'akmal@gmail.com',
-                Role: 'user',
-                KTP: '3515070000000001',
-                No_HP: '082200000001',
-                Birthday: '25-09-2002',
-                image: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-            }
+            user: Object,
+            previewImageUrl: ''
         }
     },
+
+    async created() {
+        const token = localStorage.getItem("token");
+
+        const config = {
+            headers : { Authorization : `Bearer ${token}`}
+        }
+        await axios
+            .get(env.VITE_API_URI + "/User", config)
+            .then(response => {
+                if (response.status !== 200) {
+                    console.log(repsonse);
+                }
+                else {
+                    this.user = response.data;
+                }
+            })
+            .catch(err => {console.log(err)});
+        console.log(this.user)
+        this.previewImageUrl = this.user.profilePhoto ? this.user.profilePhoto.url : ''
+    }
 }
 </script>
 
