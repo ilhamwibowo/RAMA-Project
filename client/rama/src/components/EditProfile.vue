47093aa1a87a72c0e4495f73ec05bf0df0863f6b
AkmalArifin <makmalarifin25@gmail.com>
2023-02-17 19:17:40 +0700

    Styling Commenting

--- client/rama/src/components/EditProfile.vue
+++ client/rama/src/components/EditProfile.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="profile">
         <h1>Edit Profile</h1>
+
+        <!-- Container Image -->
         <div class="container-image">
-            <input type="file" id="image" accept="image/*" @change="photoChange" style="display:none;"/>
+            <input type="file" id="image" accept="image/*" @change="changePhoto" style="display:none;"/>
             <img :src="previewImageUrl" />
             <div class="edit" @click="uploadPhoto"> 
                 <div class="circle"></div>
             </div>
         </div>
+
+        <!-- Container Detail Profile -->
         <div class="container-detail">
             <h3 class="title">Name</h3>
             <input class="input" v-model="user.Name"/>
@@ -29,22 +33,24 @@
             <input class="input" v-model="user.birthday"/>
         </div>
 
-        <!-- <router-link :to="{path: '/profile'}"> -->
-        <button class="btn save" @click="updateUser">Save</button>
-        <!-- </router-link> -->
-        <router-link :to="{path: `/profile`}">
-            <button class="btn cancel">Cancel</button>
-        </router-link>
+        <!-- Container Button -->
+        <div class="container-btn">
+            <!-- <router-link :to="{path: '/profile'}"> -->
+                <button class="btn save" @click="updateUser">Save</button>
+            <!-- </router-link> -->
+            <router-link :to="{path: `/profile`}">
+                <button class="btn cancel">Cancel</button>
+            </router-link>
+        </div>
     </div>
 </template>
 
 <script>
 import DetailProfile from './DetailProfile.vue';
 import EditDetailProfile from './EditDetailProfile.vue';
-import axios from 'axios'
+import axios from 'axios';
 
 const env = import.meta.env;
-
 
 export default {
     name: 'Profile',
@@ -52,49 +58,65 @@ export default {
         DetailProfile,
         EditDetailProfile
     },
-    props: {
-        
-    },
     data() {
         return {
             user: Object,
             previewImage: Object,
-            previewImageUrl: ''
+            previewImageUrl: '',
+            flagPhoto: false
         }
     },
     methods: {
         async updateUser() {
             const token = localStorage.getItem('token')
-
-            // Upload Photo
+            
+            // UPLOAD PHOTO
             var profilePhoto
-            var formData = new FormData();
-            // var imageFile = document.querySelector('#image')
-            formData.append('file', this.previewImage)
-            const configPhoto = {
-                headers: { 
-                    Authorization: `Bearer ${token}`,
-                    'Content-Type': 'multipart/form-data'
-                },
-            }
+            // console.log(this.previewImageUrl)
+            console.log(this.flagPhoto)
 
-            await axios
+            if (this.flagPhoto) {
+                console.log("A")
+                // Configuration for post api
+                const configPhoto = {
+                    headers: { 
+                        Authorization: `Bearer ${token}`,
+                        'Content-Type': 'multipart/form-data'
+                    },
+                }
+                
+                // Create FormData file for post api
+                var formData = new FormData();
+                formData.append('file', this.previewImage)
+                
+                // Axios Post
+                await axios
                 .post(env.VITE_API_URI + "/User/add-photo", formData, configPhoto)
                 .then(response => profilePhoto = response.data)
                 .catch(err => console.log(err))
+            } else {
+                console.log("B")
+                console.log(this.user)
+                profilePhoto = this.user.profilePhoto
+            }
 
             console.log(profilePhoto)
 
-            // Upload User
+            // UPDATE USER
+            var status
+
+            // Configuration for put api
             const configUser = {
                 headers: { 
                     Authorization: `Bearer ${token}`
                 },
             }
+
+            // Body request for put api
             const body = {
                 role: this.user.role,
                 ktp: this.user.ktp,
-                no_HP: this.user.no_hp,
+                no_HP: this.user.no_HP,
                 birthday: this.user.birthday,
                 photoUrl: profilePhoto.url,
                 profilePhoto: {
@@ -103,20 +125,34 @@ export default {
                 }
             }
             
-            // Post current {Role, KTP, No_HP, Birthday}
-            axios
+            // Axios Put
+            await axios
                 .put(env.VITE_API_URI + '/User/edit', body, configUser)
-                .then(response => this.id = response.data.id)
-                .catch(err => console.log(err))
+                .then(response => status = response.status)
+                .catch(err => {
+                    console.log(err)
+                    status = response.status
+                    }
+                )
 
+            // Status Alert
+            if (status === 200) {
+                alert("Update Profile Success")
+            } else {
+                console.log(status)
+                alert("Update Profile Failed")
+            }
+            this.$router.push("/profile")
         },
+        // File Upload
         uploadPhoto() {
             document.getElementById("image").click()
         },
-        photoChange(event) {
+        changePhoto(event) {
             const image = event.target.files[0]
             this.previewImage = image
             this.previewImageUrl = URL.createObjectURL(image)
+            this.flagPhoto = true
         }
     },
 
@@ -124,9 +160,12 @@ export default {
     async created() {
         const token = localStorage.getItem("token");
 
+        // Configuration for API
         const config = {
             headers : { Authorization : `Bearer ${token}`}
         }
+
+        // Axios Get
         await axios
             .get(env.VITE_API_URI + "/User", config)
             .then(response => {
@@ -138,7 +177,8 @@ export default {
                 }
             })
             .catch(err => {console.log(err)});
-        console.log(this.user)
+
+        // Handle if there is no photo
         this.previewImageUrl = this.user.profilePhoto ? this.user.profilePhoto.url : ''
     }
 }
