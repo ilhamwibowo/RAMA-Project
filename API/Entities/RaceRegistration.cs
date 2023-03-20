d13a09efea0b47f2c422f365f72dc543e6fb6c31
fadillfauzani <13520032@std.stei.itb.ac.id>
2023-03-20 14:49:51 +0700

    Registration Database

---
+++ API/Entities/RaceRegistration.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class RaceRegistration
+    {
+        public enum PaymentStatus
+        {
+            Waiting,
+            Paid,
+            TakenKit, //taken the Runner Kit, Changed when user take runner kit.
+            Completed //Event is Over. Changed by admin?
+        };
+        public int RaceId { get; set; }
+        public int AccId { get; set; }
+        public DateTime RegistedAt{ get; set; } = DateTime.UtcNow;
+
+        public int RegistrationFee{ get; set; }
+
+        // public Payment payment  // for later 
+        public PaymentStatus Status { get; set;} = PaymentStatus.Waiting;
+
+        public DateTime PaidAt{get; set;}
+        public DateTime TakenKitAt{get; set;}
+        
+    }
+}
