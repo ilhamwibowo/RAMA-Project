05b05c539e6f418577869bb865a0b87af906dc06
tastytypist <tastytypist@users.noreply.github.com>
2023-03-24 15:51:41 +0700

    Implement race leaderboard

--- client/rama/src/views/HomeView.vue
+++ client/rama/src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <template>
     <main>
+        <Leaderboard :attendees="raceData" />
         <LeaderboardPagination
-            :totalPage="10"
-            :perPage="10"
+            :totalPage="this.pageCount"
+            :perPage="this.perPage"
             :currentPage="currentPage"
             @pageChanged="onPageChange"
         />
@@ -10,25 +11,76 @@
 </template>
 
 <script>
+import axios from "axios";
+
+import Leaderboard from "@/components/Leaderboard.vue";
 import LeaderboardPagination from "@/components/LeaderboardPagination.vue";
 
 export default {
     name: "HomeView",
 
     components: {
+        Leaderboard,
         LeaderboardPagination
     },
 
     data() {
         return {
-            currentPage: 1
+            raceId: 1,
+            raceData: [],
+            currentPage: 1,
+            pageCount: 1,
+            perPage: 20
         };
     },
 
     methods: {
         onPageChange(page) {
             this.currentPage = page;
+            axios
+                .get(
+                    import.meta.env.VITE_API_URI +
+                        "/Race/" +
+                        this.raceId +
+                        "/attendees?page=" +
+                        this.currentPage +
+                        "&pageSize=" +
+                        this.pageCount
+                )
+                .then((response) => {
+                    this.raceData = response.data.raceAttendee;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         }
+    },
+
+    async beforeMount() {
+        this.pageCount = await axios
+            .get(import.meta.env.VITE_API_URI + "/Race/" + this.raceId + "/attendance")
+            .then((response) => {
+                return Math.ceil(response.data.raceAttendee.length / this.perPage);
+            })
+            .catch(() => {
+                return 10;
+            });
+        await axios
+            .get(
+                import.meta.env.VITE_API_URI +
+                    "/Race/" +
+                    this.raceId +
+                    "/attendees?page=" +
+                    this.currentPage +
+                    "&pageSize=" +
+                    this.pageCount
+            )
+            .then((response) => {
+                this.raceData = response.data.raceAttendee;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 };
 </script>
