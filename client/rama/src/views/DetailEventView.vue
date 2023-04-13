8a8720538813f7a072a42c00393a0eb2ae0fb7f7
Muhammad Akmal Arifin <makmalarifin25@gmail.com>
2023-04-13 11:50:54 +0700

    feat: admin event detail edit

--- client/rama/src/views/DetailEventView.vue
+++ client/rama/src/views/DetailEventView.vue
@@ -1,97 +1,8 @@
 <template>
     <div class="detail-event">
         <AdminSidebar class="sidebar"/>
+        <AdminEventDetailEdit v-if="showForm" />
         <div class="layout">
-            <form v-if="showForm">
-                   <div class="container-image">
-                    <label for="image">
-                        <img :src="previewImageUrl" />
-                    </label>
-                    <input
-                            type="file"
-                            id="image"
-                            accept="image/*"
-                            @change="changePhoto"
-                        />
-                   </div>
-
-                   <div class="form-grid">
-                       <!-- Left column -->
-                       <div class="grid-item">
-                           <label for="name">Name</label>
-                           <input type="text" id="name" v-model="name" >
-                           
-                           <label for="city">City</label>
-                           <input type="text" id="city" v-model="city">
-                           
-                           <label for="start-date">Start Date</label>
-                           <input type="datetime-local" id="start-date" v-model="startDate">
-                           
-                       </div>
-                       
-                       <!-- Right column -->
-                       <div class="grid-item">
-                           <label for="startlocation">Description</label>
-                           <input type="text" id="startlocation" v-model="description">
-
-                           <label for="latitude">Start Registration</label>
-                           <input type="date" id="latitude" v-model="startRegis">
-
-                           <label for="longitude">End Registration</label>
-                           <input type="date" id="longitude" v-model="endRegis">
-
-                       </div>
-                       
-                       <!-- Bottom row -->
-                       <div class="grid-item">
-                           <div class="row">
-                               <div class="row-item">
-                                   <label for="course-map">Album</label>
-                                   <!-- <input type="text" id="course-map" v-model="albumid" placeholder="TBI"> -->
-                                   <select id="albumid" v-model="albumId">
-                                        <option value="">Pilih Album</option>
-                                        <option v-for="namaAlbum in this.album" :value="namaAlbum.albumId">{{ namaAlbum.albumName }}</option>
-                                    </select>
-                                    <p>{{ this.albumid }}</p>
-                               </div>
-
-                           </div>           
-                           
-
-                       </div>
-
-                       <div class="grid-item">
-                           <div class="row">
-                               <label for="isPublish">Published</label>
-                               <input id="isPublish" type="checkbox" v-model="isPublish">
-                           </div>
-                       </div>
-
-                       <div class="grid-item">
-                           <div class="row">
-                               <div class="row-item"> 
-                                   <label for="distance"> Distance</label>
-                                   <input type="number" id="distance" v-model="distance">
-                               </div>
-
-                               <div class="row-item"> 
-                                   <label for="price">Price</label>
-                                   <input type="number" id="price" v-model="price" >
-                               </div>
-                           </div>
-                       </div>
-
-                       <!-- <div class="grid-item"></div> -->
-                       <div class="grid-item">
-                           <div class="button-container"> 
-                               <button class="btn-cancel" @click="toggleForm">CANCEL</button>
-                               <button class="btn-save" @click.prevent="saveEvent">SAVE</button>
-                           </div> 
-                       </div>                      
-                       </div>
-               </form>
-                <div class="overlay" v-if="showForm"></div>
-
             <div class="buttons-container">
                 <div class="edit-button-container">
                     <button class="edit-button" @click="toggleForm()" >EDIT</button> 
@@ -181,11 +92,14 @@
 
 <script>
 import axios from 'axios';
-import AdminSidebar from '../components/AdminSidebar.vue';
+import AdminSidebar from '@/components/AdminSidebar.vue';
+import AdminEventDetailEdit from '@/components/AdminEventDetailEdit.vue';
+
 export default {
     name: "detailEvent",
     components: {
-        AdminSidebar
+        AdminSidebar,
+        AdminEventDetailEdit
     },
     data(){
         return {
@@ -193,15 +107,7 @@ export default {
             album: [],
             id :this.$route.params.id,
             showForm: false,
-            name: "", 
-            city: "", 
-            startDate: "",
-            startLocation: "",
-            latitude: "",
-            longitude: "",
-            courseMap: "",
             isOpen: false,
-            isPublish:false,
             previewImageUrl: "",
             basePreviewImageUrl:"", 
             showDeleteForm : false,
@@ -209,7 +115,6 @@ export default {
             startRegis: "",
             endRegis: "",
             albumId: "",
-            price: "",
             photo:Object
         };
     },
@@ -559,31 +464,6 @@ h2 {
     color: #000;
 }
 
-.category-container {
-    width: 50%;
-    left: 25%;
-    margin-bottom: 2.5rem;
-}
-
-.category-table {
-    border-collapse: collapse;
-    text-align: center;
-    margin-bottom: 1rem;
-    width: 100%;
-    table-layout: fixed;
-    color: #000;
-}
-
-.table-row-header {
-    border-bottom: 2px solid #272626;
-    font-family: "Montserrat", sans-serif;
-    font-size: 1.25rem;
-}
-
-.table-row-body {
-    font-size: 1.25rem;
-}
-
 .maps {
     margin-bottom: 10%;
     height: 30%;
@@ -594,122 +474,7 @@ h2 {
     height: auto;
     border-radius: 15px;
 }
-
-form {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: white;
-  padding: 20px;
-  z-index: 999;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-  text-align: left;
-}
-
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  z-index: 998;
-}
-
-.form-grid {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-column-gap: 20px;
-    grid-row-gap: 20px;
-
-  }
   
-.form-grid .grid-item {
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-row-gap: 10px;
-}
-  
-
-  /* Optional styling for labels and inputs */
-label {
-    font-weight: bold;
-}
-  
-input {
-    width: 100%;
-    padding: 5px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-  }
-
-  .form-grid input[type=text], 
-  .form-grid input[type=number],
-  .form-grid input[type=datetime-local] {
-    border: 2px solid #5f5f5f;
-    padding: 5px 10px;
-    width: 100%;
-    border-radius: 20px;
-    border: 2px solid grey;
-    color: rgb(63, 62, 62);
-    font-size: 20px;
-  }
-
-  .form-grid label {
-    margin-left:10px;
-  }
-  
-.form-grid button {
-    width: 100px;
-    height: 40px;
-    padding: 10px;
-    border-radius: 30px;
-    cursor: pointer;
-    font-family: 'Montserrat', sans-serif;
-    font-weight: bold;
-    transition: background-color 0.2s;
-    margin: 5px;
-}
-
-.btn-save {
-    background-color: #1f2220;
-    color: #fff;
-}
-.btn-cancel {
-    background-color: transparent;
-    color: #000000;
-    border:2px solid grey;
-}
-.btn-save:hover  {
-  background-color: #171817;
-}
-.btn-cancel:hover {
-    background-color: #b5b9b5
-}
-.row {
-  display: flex;
-  align-items: center;
-  flex-wrap: nowrap;
-  gap:10px;
-}
-
-.button-container {
-    display: flex;
-    justify-content: flex-end;
-    align-items: right;
-    flex-wrap: nowrap;
-    gap:10px;
-}
-.row label {
-  margin-right: 10px;
-}
-.row input {
-  flex-grow: 1;
-}
-
-
 .delete-form-overlay {
   position: fixed;
   top: 0;
@@ -778,15 +543,6 @@ input {
   color: white;
 }
 
-.container-image {
-    display: flex;
-    margin: 0 auto;
-    max-width: 400px;
-    align-items: center;
-    overflow: hidden;
-    text-align: center;
-    justify-content: center;
-}
 
 #image {
   position: absolute;
