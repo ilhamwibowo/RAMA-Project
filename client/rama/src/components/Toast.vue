6b0e31c6158c26d57cb3f9d96ca95ec72091c60d
Muhammad Akmal Arifin <makmalarifin25@gmail.com>
2023-04-27 23:44:21 +0700

    feat: toast component

--- client/rama/src/components/Toast.vue
+++ client/rama/src/components/Toast.vue
@@ -1,14 +1,19 @@
 <template>
     <div class="toast-wrapper">
-        <div class="toast">
-            This is a toast.
+        <div class="toast" :id="type">
+            {{ message }}
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    name: "Toast",
+    props: {
+        type: String,
+        message: String
+    }
+
 }
 </script>
 
@@ -19,12 +24,21 @@ export default {
     top: 20px;
 }
 .toast {
+    font-family: "Darker Grotesque";
+    font-size: 20px;
     padding: 20px;
     color: white;
-    background: #ff0062;
     border-radius: 10px;
     box-shadow: 1px 3px 5px rgba(0,0,0,0.2);
     max-width: 400px;
     margin: 0 auto;
+}
+
+#error {
+    background: #ff4444;
+}
+
+#success {
+    background: #00C851;
 }
 </style>
