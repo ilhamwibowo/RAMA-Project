5605aecb722a7d68993bf5a517fdf61b948d0d46
fadillfauzani <13520032@std.stei.itb.ac.id>
2023-04-29 12:09:04 +0700

    cf-ru5102 reader

---
+++ ChafonReaderApi-master/Rmg.Chafon.ReaderApi/Ru5102/Ru5102Device.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rmg.Chafon.ReaderApi.Ru5102
+{
+    public class Ru5102Device : IDisposable
+    {
+        private readonly Ru5102SerialPort Port;
+        private GetReaderInformationResponse Info;
+
+        #region ctor
+
+        internal Ru5102Device(Ru5102SerialPort port, GetReaderInformationResponse info)
+        {
+            this.Port = port;
+            this.Info = info;
+        }
+
+        public static async Task<Ru5102Device> CreateAsync(string comPort, int baudRate, byte address)
+        {
+            var port = new Ru5102SerialPort(comPort, baudRate, address);
+            try
+            {
+                using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5)))
+                {
+                    var info = await port.SendReceiveAsync(new GetReaderInformationRequest(), cts.Token);
+                    return new Ru5102Device(port, info);
+                }
+            }
+            catch
+            {
+                port.Dispose();
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            Port.Dispose();
+        }
+
+        #endregion
+
+        public async Task InventoryAsync(AddressSegment? tidAddress, Action<string> handleTag, CancellationToken ct = default)
+        {
+            await Port.SendReceiveUntilAsync(new InventoryRequest(tidAddress), resp =>
+            {
+                foreach (var tag in resp.Tags)
+                {
+                    handleTag(tag);
+                }
+
+                return !resp.ScanFinished;
+            }, ct);
+        }
+
+        public async Task<byte[]> ReadMemoryAsync(
+    string tag, uint password,
+    MemoryBank bank, AddressSegment seg,
+    CancellationToken ct = default
+)
+        {
+            var resp = await TryReadMemoryAsync(tag, password, bank, seg, ct);
+            if (!resp.Success)
+            {
+                throw new NakException("Tag did not respond");
+            }
+            return resp.Data;
+        }
+
+        public async Task<ReadResult> TryReadMemoryAsync(
+            string tag, uint password,
+            MemoryBank bank, AddressSegment seg,
+            CancellationToken ct = default
+        )
+        {
+            var resp = await Port.SendReceiveAsync(new ReadRequest(tag, password, bank, seg), ct);
+            return new ReadResult(resp.Status == ReadStatus.Success, resp.Data);
+        }
+    }
+
+    public enum MemoryBank : byte
+    {
+        Reserved = 0,
+        EPC = 1,
+        TID = 2,
+        User = 3,
+    }
+
+    public record AddressSegment(int Offset, int Length);
+
+    public record ReadResult(bool Success, byte[] Data);
+
+    [Serializable]
+    public class NakException : Exception
+    {
+        public NakException() { }
+        public NakException(string message) : base(message) { }
+        public NakException(string message, Exception inner) : base(message, inner) { }
+        protected NakException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ ChafonReaderApi-master/TestConsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using Rmg.Chafon.ReaderApi.Ru5102;
+using System;
+
+var com = 0;
+var readInput = "";
+do
+{
+    Console.WriteLine("masukkan Com berapa Hardware berada! {1-9}");
+    readInput = Console.ReadLine();
+
+}while (!Int32.TryParse(readInput, out com) && com < 10 && com > 0);
+
+try
+{
+    using var reader = await Ru5102Device.CreateAsync("COM" + com, 57600, 0x00);
+    Console.WriteLine("Type 'End' to Close");
+    Console.WriteLine("Type Any key to Scan");
+    while (Console.ReadLine() != "End")
+    {
+        Console.WriteLine("Dekatkan Tag dengan Hardware untuk Membaca!\n");
+        var prev = "";
+        while (true)
+        {
+            var tags = string.Empty;
+            await reader.InventoryAsync(null, tag => tags = tag);
+            if (prev == tags) continue;
+            var readResp = await reader.TryReadMemoryAsync(tags, 0, MemoryBank.Reserved, new AddressSegment(4, 4));
+            prev = tags;
+            if (tags != "")
+            {
+                Console.WriteLine($"Read : {tags}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Read NAK");
+            }
+        }
+        Console.WriteLine("Type 'End' to Close");
+        Console.WriteLine("Type Any key to ReScan");
+    }
+    
+}
+catch
+{
+    Console.WriteLine("Masukkan Hardwarenya");
+    Console.ReadLine();
+    System.Environment.Exit(0);
+}
